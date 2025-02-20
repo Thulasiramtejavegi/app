303ad17f389b3552a6c3efecd3052f4bcbde6bf8
grooveinnovations-thulasiramteja <thulasiramteja.vegi@grooveinnovations.ai>
2025-02-20 22:21:20 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,4 +7,4 @@ def home():
     return "Hello, Welcome to Groove Innovations! An AI and ML Learning platform company"
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000)
+    app.run(host='0.0.0.0', port=8080)
